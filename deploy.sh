97c4b7eebd8a6444f0957379333ba614aaca8eda
lizlove <elizabethlovero@gmail.com>
2020-10-15 14:11:04 -0400

    Update ecosystem file with prod configs and create deploy shells script.

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git pull origin master
+npm install
+npm run build
+pm2 reload ecosystem.config.js --env production
+
+# EOF
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,16 @@
 module.exports = {
-  apps : [{
-    script: 'dist/app.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+  apps: [
+    {
+      name: 'indico-slack-bot',
+      script: './dist/app.js',
+      instances: 'max',
+      max_memory_restart: '256M',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
     }
-  }
+  ]
 };
